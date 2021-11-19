73c1eb396db479014bee6a91f37bc9a2600ef457
Lucas Bevilaqua Araujo Oliveira <laobevilaqua@outlook.com>
2021-11-19 11:37:19 -0300

    chore: initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  exempleExecuteScript() {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const id = tabs[0].id;
+      if (id == null) return;
+      chrome.tabs.executeScript(id, {
+        code: 'console.log(\'oi\')',
+      });
+    });
+  }
+}
